63ae2e05b207360e03eadb3d45c1f702edd76f99
Sergio <grey_f@inbox.ru>
2021-05-24 18:50:39 +0300

    adding routs for user cotroller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   get '/pages', to: 'pages#index'
+  get '/users/new', to: 'users#new'
+  post '/users', to: 'users#create'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
